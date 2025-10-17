c52e09564cc9d28810a4d3f21a6d21e96fc26231
Jonathan Gamble <jdgamble555@gmail.com>
2025-10-17 07:34:34 -0500

    meta

--- src/app/components/about-data/about-data.resolver.ts
+++ src/app/components/about-data/about-data.resolver.ts
@@ -1,4 +1,5 @@
 import { inject, isDevMode } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ResolveFn } from '@angular/router';
 import { FIRESTORE_GET_DOC } from '@lib/firebase/firebase.service';
 import { useAsyncTransferState } from '@lib/utils';
@@ -10,11 +11,17 @@ export type AboutDoc = {
 
 export const aboutDataResolver: ResolveFn<AboutDoc> = async () => {
 
+    const getDoc = inject(FIRESTORE_GET_DOC);
+
+    const meta = inject(Meta);
+    const title = inject(Title);
+
     return useAsyncTransferState('about', async () => {
 
-        const getDoc = inject(FIRESTORE_GET_DOC);
-
         const data = await getDoc<AboutDoc>('/about/ZlNJrKd6LcATycPRmBPA');
+
+        title.setTitle(data.name);
+        meta.updateTag({ name: 'description', content: data.description });
 
         if (isDevMode()) {
             console.log(data);
